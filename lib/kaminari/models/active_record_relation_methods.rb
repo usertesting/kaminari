5ff7c791222a081615a5dbf77add9556f4c4a460
Ryan Platte <ryan.platte@gmail.com>
2013-09-09 17:29:17 -0400

    Fix enormous result set being sent and loaded into objects for pagination counts. Relies on subquery so may not be a general solution, but should be reliable on MySQL.

--- lib/kaminari/models/active_record_relation_methods.rb
+++ lib/kaminari/models/active_record_relation_methods.rb
@@ -19,7 +19,7 @@ module Kaminari
         # a workaround to count the actual model instances on distinct query because count + distinct returns wrong value in some cases. see https://github.com/amatsuda/kaminari/pull/160
         uses_distinct_sql_statement = c.to_sql =~ /DISTINCT/i
         if uses_distinct_sql_statement
-          c.length
+          ActiveRecord::Base.connection.select_one('select count(*) as c from (' + c.to_sql + ') q')['c']
         else
           # .group returns an OrderdHash that responds to #count
           c = c.count
